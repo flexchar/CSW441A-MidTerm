34809ad57a43c636259c05c58a63f9774ed0ba50
Lukas <lukas@youtuberis.lt>
2020-04-24 17:37:54 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using LukasVanagas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LukasVanagas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                id = "10",
+                firstName = "Demetris",
+                lastName = "Kyriacou"
+            },
+            new Employee
+            {
+                id = "20",
+                firstName = "Kostas",
+                lastName = "Aristotelous"
+            }
+        };
+
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Employees List";
+            ViewBag.Action = "Index";
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Add New Employee";
+            ViewBag.Action = "Create";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            employees.Add(employee);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(string id)
+        {
+            ViewBag.Title = "Update Employee";
+            return View(employees.First(e => e.id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Update(Employee employee)
+        {
+            employees.RemoveAll(e => e.id == employee.id);
+            employees.Add(employee);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Destroy()
+        {
+            ViewBag.Title = "Delete Employee";
+            ViewBag.Action = "Destroy";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Destroy(string id)
+        {
+            employees.RemoveAll(e => e.id == id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LukasVanagas.Models
+{
+    public class Employee
+    {
+        [DisplayName("Empoyee ID")]
+        [Required(ErrorMessage = "An employee must have an id")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Employee ID must a 2-digit number")]
+        public string id { get; set ; }
+
+        [DisplayName("Employee Name")]
+        [Required(ErrorMessage = "An employee must have a name")]
+        public string firstName { get; set; }
+
+        [DisplayName("Employee Surname")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "The surname must consist of 5 to 20 characters")]
+        public string lastName { get; set; }
+
+
+    }
+}
